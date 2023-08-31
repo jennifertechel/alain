a30000ed89adb57ff9fee02c1fc359ff52e51e07
Jennifer Techel <jennifertechel@hotmail.com>
2023-08-31 07:41:53 +0200

    add theme and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Products from "./components/Products";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#f7fafc",
+      200: "#CED3BF",
+      300: "#697F72",
+      400: "#F0F1ED",
+    },
+  },
+  fonts: {
+    heading: "bodoni",
+  },
+});
 
 function App() {
   return (
-    <>
-      <div className='App'>
-        <div className='Hero'>
-          <div className='TextBox'>
-            <h1 style={{ fontSize: "3rem" }}>
-              TEXT HERE <br /> AND HERE
-            </h1>
-          </div>
-        </div>
-      </div>
-    </>
+    <ChakraProvider theme={theme}>
+      <Header />
+      <Hero />
+      <About />
+      <Products />
+      <Contact />
+      <Footer />
+    </ChakraProvider>
   );
 }
 
